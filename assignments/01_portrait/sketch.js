1c21070d79a8cd7eeacf91460801ef6582e0315d
LoveMachineY <yh3548a@student.american.edu>
2024-01-25 18:40:39 -0500

    Update sketch.js
    new update

--- assignments/01_portrait/sketch.js
+++ assignments/01_portrait/sketch.js
@@ -1,22 +1,43 @@
-let hue = 0;
-let x;
-let y;
+let thing;
 
 function setup() {
-  createCanvas(400, 300);
-  
-  x= width/2;
-  y = height/2;
-  
+  createCanvas(800, 600);
   colorMode(HSB);
+
+  thing = new Thing(100, 100);
+  thing2 = new Thing(200, 200)
 }
 
 function draw() {
-  background (0, 0, 100);
-  hue += 5; //hue = hue + 1;
-  let saturation = mouseX / width * 100;
+  background(0, 0, 100);
+
+  thing.update();
+  thing2.update2();
+}
+
+
+class Thing {
+  constructor(x, y) {
+    this.hue = 0;
+
+    this.position = createVector(x, y);
+    this.velocity = createVector(random(-5, 5), random(-5, 5));
+  }
+
+  update() {
+
+    this.hue += 5; // hue = hue + 1;
+    let saturation = mouseX / width * 100;
   
-  fill(hue % 360, saturation, 100);
+    fill(this.hue % 360, saturation, 100);
+    circle(this.position.x, this.position.y, 100);
+  }
+
+  update2(){
+    this.hue += 3; // hue = hue + 1;
+    let saturation = mouseX / width * 100;
   
-  circle(x, y, 100);
+    fill(this.hue % 360, saturation, 100);
+    circle(this.position.x, this.position.y, 100);
+  }
 }
