2b8aa4bb81462c7d88831460cee2ea2599ff2430
LoveMachineY <yh3548a@student.american.edu>
2024-02-29 11:25:31 -0500

    update

--- assignments/05_grid/sketch.js
+++ assignments/05_grid/sketch.js
@@ -1,29 +1,29 @@
 function setup() {
-    createCanvas(400, 400);
-    noiseDetail(24); 
-  }
+  createCanvas(400, 400);
+  noiseDetail(24); 
+}
+
+function draw() {
+  background(220);
+  let gridSize = 10; 
+  let cellSize = width / gridSize;
   
-  function draw() {
-    background(220);
-    let gridSize = 10; 
-    let cellSize = width / gridSize;
-    
-    for (let x = 0; x < gridSize; x++) {
-      for (let y = 0; y < gridSize; y++) {
-        
-        let noiseScale = 0.05; 
-        let noiseVal = noise(x * noiseScale, y * noiseScale);
-        
-        
-        let dynamicSize = sin(frameCount * 0.1 + noiseVal * PI) * 25 + 50;
-        
-        let posX = x * cellSize + cellSize / 2;
-        let posY = y * cellSize + cellSize / 2;
-        
-        
-        ellipse(posX, posY, dynamicSize, dynamicSize);
-      }
+  for (let x = 0; x < gridSize; x++) {
+    for (let y = 0; y < gridSize; y++) {
+      
+      let noiseScale = 0.05; 
+      let noiseVal = noise(x * noiseScale, y * noiseScale);
+      
+      
+      let dynamicSize = sin(frameCount * 0.1 + noiseVal * PI) * 25 + 50;
+      
+      // Calculate position
+      let posX = x * cellSize + cellSize / 2;
+      let posY = y * cellSize + cellSize / 2;
+      fill(255,dynamicSize);
+      
+      rect(posX, posY, dynamicSize, dynamicSize);
     }
-    
   }
   
+}
