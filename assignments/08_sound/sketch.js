c88113b33855ce240c53906e825fb0d0b1b5e0e0
LoveMachineY <yh3548a@student.american.edu>
2024-04-17 14:02:11 -0400

    update

--- assignments/08_sound/sketch.js
+++ assignments/08_sound/sketch.js
@@ -1,22 +1,18 @@
 let song;
 
-function preload() {
-  soundFormats('ogg', 'mp3');
-  song = loadSound("https://lovemachinex.github.io/computional-art-spring2024/assignments/08_sound/sample");
-}
-
 function setup() {
-  createCanvas(710, 200);
-  song.play();
-  background(0, 255, 0);
+  song = loadSound('https://lovemachinx.github.io/computational-art-spring2024/assignments/08_sound/samples/drum.wav');
+  createCanvas(720, 200);
+  background(255, 0, 0);
 }
 
 function mousePressed() {
   if (song.isPlaying()) {
-    song.pause();
+    // .isPlaying() returns a boolean
+    song.stop();
     background(255, 0, 0);
   } else {
-    song.play(); 
+    song.play();
     background(0, 255, 0);
   }
 }
