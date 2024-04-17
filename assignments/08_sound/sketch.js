fcf0e07419e67ac15375339a49a55ae1cbd5c1a6
LoveMachineY <yh3548a@student.american.edu>
2024-04-17 14:06:25 -0400

    Update sketch.js

--- assignments/08_sound/sketch.js
+++ assignments/08_sound/sketch.js
@@ -1,7 +1,7 @@
 let song;
 
 function setup() {
-  song = loadSound('https://lovemachinx.github.io/computational-art-spring2024/assignments/08_sound/samples/drum.wav');
+  song = loadSound('https://lovemachinex.github.io/computational-art-spring2024/assignments/08_sound/samples/drum.wav');
   createCanvas(720, 200);
   background(255, 0, 0);
 }
