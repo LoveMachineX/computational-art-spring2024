ea29b0023f8d5f3639212e4f200eafc8b998720f
LoveMachineY <yh3548a@student.american.edu>
2024-05-05 20:16:32 -0400

    Update player.js

--- assignments/final/player.js
+++ assignments/final/player.js
@@ -20,7 +20,7 @@ class Player {
 
     update(obstacles) {
         if (this.movingUp && !this.collided) {
-            this.y -= 10;
+            this.speedY = -10;
         }
         if (this.movingDown && !this.collision(this.x, this.y + 1, obstacles)) {
             this.y += 5;
