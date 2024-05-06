81f2f276b9705cbd169bc60dba4dac6840af7944
LoveMachineY <yh3548a@student.american.edu>
2024-05-05 22:58:55 -0400

    Update sketch.js

--- assignments/final/sketch.js
+++ assignments/final/sketch.js
@@ -39,10 +39,9 @@ function draw() {
     img.loadPixels();
     obstacles = [];
     processVideo();
-    gold.placeRandomly(obstacles, width, height);
-    gold.draw();
   } else {
     image(staticImage, 0, 0, width, height);
+    gold.draw();
   }
   player.update(obstacles);
   player.draw();
@@ -97,6 +96,7 @@ function toggleFreeze() {
   freeze = !freeze;
   if (freeze) {
       staticImage = get();  // Capture the current canvas as the static image
+      gold.placeRandomly(obstacles, width, height); // Place gold square randomly on freeze
       if (!player.windChangeTimer) {
         player.windChangeTimer = 300;  // Start with 5 seconds after freeze
     }
